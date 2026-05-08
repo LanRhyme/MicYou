4286d6a4828533a0529d49161ed2133d6591d8eb
ChinsaaWei <chinsaa@163.com>
2026-05-09 03:37:56 +0800

    fix(android): 添加缺失的 Modifier 导入

--- composeApp/src/androidMain/kotlin/com/lanrhyme/micyou/Platform.android.kt
+++ composeApp/src/androidMain/kotlin/com/lanrhyme/micyou/Platform.android.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
