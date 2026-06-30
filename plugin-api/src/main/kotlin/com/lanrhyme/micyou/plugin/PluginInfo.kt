3444c6a316078aa5c8dd606f54dc207050a2dcf2
ChouChiu <lshengevery@gmail.com>
2026-07-01 00:34:43 +0800

    clean code 1

--- plugin-api/src/main/kotlin/com/lanrhyme/micyou/plugin/PluginInfo.kt
+++
@@ -1,9 +0,0 @@
-package com.lanrhyme.micyou.plugin
-
-data class PluginInfo(
-    val manifest: PluginManifest,
-    val isEnabled: Boolean = false,
-    val isLoaded: Boolean = false,
-    val installPath: String,
-    val iconPath: String? = null
-)
